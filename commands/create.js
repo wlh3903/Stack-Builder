ee12ed841326def49cd5b26aff2557dfa865e76a
Warner Harper <warner17@Warners-Laptop-1235.local>
2026-07-04 13:11:43 -0400

    CREATE: cleaned code

--- commands/create.js
+++ commands/create.js
@@ -3,36 +3,45 @@ import fs from "fs-extra";
 import path from "path";
 import { execa } from 'execa';
 
+async function getAppName() {
+    return await text({
+        message: 'What is the name of your application?',
+        placeholder: 'my-mern-app',
+        validate(value) {
+            if (!value) return 'Name cannot be empty';
+        },
+    });
+}
+
+async function initializeViteClient(clientOriginalPath, CWD, clientPath) {
+    await execa('npx', [
+        'create-vite@latest',
+        clientOriginalPath,
+        '--',
+        '--template',
+        'react-ts',
+        ], {
+        stdout: 'ignore',
+        stderr: 'inherit',
+    })
+    .then(async () => {
+        await fs.move(path.join(CWD, clientOriginalPath), clientPath);
+    });
+}
+
 export default function registerCreateCommand(program) {
     const CWD = process.cwd();
     const isDevelopment = process.env.NODE_ENV === 'development';
+    const clientPath = path.join(CWD, (isDevelopment ? `/fake/client` : `client`));
+    const serverPath = path.join(CWD, (isDevelopment ? '/fake/server' : '/server'));
+
     program.command('create')
     .description('Create a new MERN stack application')
     .action(async () => {
         intro('Creating your MERN application...');
-        const name = await text({
-            message: 'What is the name of your application?',
-            placeholder: 'my-mern-app',
-            validate(value) {
-                if (!value) return 'Name cannot be empty';
-            },
-        });
-        const clientPath = path.join(CWD, (isDevelopment ? `/fake/client` : `client`));
-        const serverPath = path.join(CWD, (isDevelopment ? '/fake/server' : '/server'));
-        const reactOriginalPath = (isDevelopment ? `/fake/${name}` : name)
-        await execa('npx', [
-                'create-vite@latest',
-                reactOriginalPath,
-                '--',
-                '--template',
-                'react-ts',
-                ], {
-                stdout: 'ignore',
-                stderr: 'inherit',
-            })
-            .then(async () => {
-                await fs.move(path.join(CWD, reactOriginalPath), clientPath);
-            });
+        const name = await getAppName();
+        const clientOriginalPath = (isDevelopment ? `/fake/${name}` : name)
+        await initializeViteClient(clientOriginalPath, CWD, clientPath);
         log.step(`Created client directory at ${clientPath}`);
         await fs.ensureDir(serverPath);
         log.step(`Created server directory at ${serverPath}`);
